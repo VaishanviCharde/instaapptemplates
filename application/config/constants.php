f3e0a961c00ba6e092660d508c0cb4ab764fdcbf
VaishanviCharde <159644048+VaishanviCharde@users.noreply.github.com>
2024-04-02 14:07:38 +0530

    Web constant.php file update via Python script

--- application/config/constants.php
+++ application/config/constants.php
@@ -97,7 +97,7 @@ define("URL", "https://restaurant60-be-dev-xtpocjmkpa-uw.a.run.app/");
 //     define("TEMPCOLOR", ''); //#356AB1 
 // }
 
-define("RESTID", '795'); // Restaurant - 280 , UsedCar -- 239 , dev-veges - 645
+define("RESTID", '796'); // Restaurant - 280 , UsedCar -- 239 , dev-veges - 645
 define("TEMPNAME", 'template21'); // Resale --> template 1-10, Restaurant -> template 11-20 , Veges -> 21-30
 define("TEMPCOLOR", ''); //#356AB1 
 // define("Logo", ''); //#356AB1 
